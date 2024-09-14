eb7939e36677b583ba4412ca77456c70cc4090c2
Eduardo Tribst <eduardo.tribst@gmail.com>
2024-09-14 19:51:08 -0300

    +- arrumada bob extends enemy

--- game/enemies/bob.js
+++ game/enemies/bob.js
@@ -1,9 +1,10 @@
 class Bob extends Enemy {
     constructor(x, y){
-        super(x, y, "./Assets/Bob-spritesheet.png", 50, 80, 15, 0, 100, 10);
+        super({x, y}, "./Assets/Bob-spritesheet.png", 50, 80, 15, 0, 100, 10);
     }
 
     update(){
         super.draw();
+        
     }
 }
