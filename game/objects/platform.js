e9f79a4b6da81b368fbaf9d273e4f734f37cd34b
EduardoTribst <eduardo.tribst@gmail.com>
2024-09-30 09:37:15 -0300

    imagem plataforma implementado

--- game/objects/platform.js
+++ game/objects/platform.js
@@ -1,9 +1,14 @@
 class Platform {
-    constructor({x, y, width, height} /*imgSource*/) {
-        //const image = new Image()
-        //this.image = image
-        //this.image.src = imgSource;
-
+    constructor({x, y, width, height}) {
+        const image = new Image()
+        image.src = "./Assets/spriteFloor/floor1.png";
+        this.image = image
+        
+        this.isImageLoaded = false
+        this.image.onload = () => {
+            this.isImageLoaded = true
+        }
+ 
         this.velocity = {// velocity in each axis
             x: 0
         };
@@ -15,17 +20,18 @@ class Platform {
         this.width = width;
         this.height = height;
 
-        this.position = {x, y};
+        this.position = {
+            x: x,
+            y: y
+        };
+
         this.position2 = {
             x: this.position.x + this.width,
             y: this.position.y + this.height
         }
+
     }
     
-    
-         
-        
-
     move(direction){ // player movement trick
         if (direction === "r"){
             this.velocity.x = this.speed.x;
@@ -37,13 +43,27 @@ class Platform {
     }
 
     draw() {
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        //ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if (this.isImageLoaded){ 
+            const pattern = ctx.createPattern(this.image, "repeat")
+            ctx.rect(this.position.x, this.position.y, this.width, this.height)
+            ctx.fillStyle = pattern
+            ctx.fill()
+        }
     }
+
+    updatePlayerPositions(x, y){
+        this.position.x = x
+        this.position.y = y
+    }
+
     update() {
-        this.draw()
+        //this.updatePlayerPositions(x, y)
+        this.draw() //DESCOMETAR O FILLRECT E COMENTAR O RESTO PARA DESCRASHAR
         this.position2 = {
             x: this.position.x + this.width,
             y: this.position.y + this.height
         }
+        
     }
 }
